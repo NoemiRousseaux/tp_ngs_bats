93a95e121897ddc3ddb833f7e7918f69df2260d7
NoemiRousseaux <noemi.rousseaux@ens-lyon.fr>
2020-11-18 08:27:37 +0000

    Analyse fastqc des sequences nettoyees sorties de Trimmomatic

---
+++ Script/programfqcposttrim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd /home/rstudio/data/mydatalocal/tp_ngs_bats/Script
+
+#Quality test of sequences
+
+# Dossier par defaut
+data="/home/rstudio/data/mydatalocal/data"
+mkdir -p $data
+cd $data
+
+# Dossier specifique a ce programme pour l'output
+data_fastqc="fastqc_data2"
+mkdir -p $data_fastqc
+cd $data_fastqc
+
+#Recuperation des données 
+home_trimm="/home/rstudio/data/mydatalocal/donnees/trimm_data"
+trimm=$home_trimm/"*.gz"
+
+#Loop for to run fastqc on every read
+for sample in $trimm
+do 
+fastqc -o $data/$data_fastqc $sample
+done
