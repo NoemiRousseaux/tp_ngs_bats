37b1ffac9b3588a27e5cdf2ac5e53f0df6f01dd3
NoemiRousseaux <noemi.rousseaux@ens-lyon.fr>
2020-11-18 08:13:54 +0000

    Trimmomatic envoye dans le bon dossier normalement maintenant

--- programtrimmomatic.sh
+++ programtrimmomatic.sh
@@ -11,21 +11,16 @@ cd $data
 data_trimm="trimm_data"
 mkdir -p $data_trimm
 cd $data_trimm
+trimm=$data/"trimm_data"
 
 #Recuperation des données 
 reads=$data/"FASTQ"
-libs="${reads}/Lib1_31_20_S1
-${reads}/Lib2_31_20_S2
-${reads}/Lib3_31_20_S3
-${reads}/Lib4_31_20_S4
-${reads}/Lib5_31_20_S5
-${reads}/Lib6_31_20_S6"
 
 
 #Loop for to run trimmomatic on every read
-for lib in $libs
-do 
-java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 8 -phred33 ${lib}_R1_001.fastq.gz ${lib}_R2_001.fastq.gz ${lib}paired1.fq.gz ${lib}unpaired1.fq.gz ${lib}paired2.fq.gz ${lib}unpaired2.fq.gz ILLUMINACLIP:$data/adapt.fasta:2:30:10 HEADCROP:9 MINLEN:100
+for n in 1 2 3 4 5 6 
+do
+java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 8 -phred33 ${reads}/Lib{n}_31_20_S{n}_R1_001.fastq.gz ${reads}/Lib{n}_31_20_S{n}_R2_001.fastq.gz ${trimm}Lib{n}paired1.fq.gz ${trimm}Lib{n}paired2.fq.gz ${trimm}Lib{n}unpaired1.fq.gz ${trimm}Lib{n}unpaired2.fq.gz ILLUMINACLIP:$data/adapt.fasta:2:30:10 HEADCROP:9 MINLEN:100
 
 done
 
